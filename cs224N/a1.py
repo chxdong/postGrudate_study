a6baf4d59b30fd78a4840d6601fba30d496ca7b1
躺平加菲猫 <10709382+lai_cat@user.noreply.gitee.com>
2025-09-11 21:32:22 +0800

    0911

---
+++ cs224N/a1.py
@@ -0,0 +1,43 @@
+# All Import Statements Defined Here
+# Note: Do not add to this list.
+# All the dependencies you need, can be installed by running .
+# ----------------
+
+import sys
+
+assert sys.version_info[0] == 3
+assert sys.version_info[1] >= 5
+
+from gensim.models import KeyedVectors
+from gensim.test.utils import datapath
+import pprint
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = [10, 5]
+import nltk
+
+nltk.download('reuters')
+from nltk.corpus import reuters
+import numpy as np
+import random
+import scipy as sp
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA
+
+START_TOKEN = ''
+END_TOKEN = ''
+
+np.random.seed(0)
+random.seed(0)
+# ----------------
+
+def read_corpus(category="crude"):
+    """ Read files from the specified Reuter's category.
+        Params:
+            category (string): category name
+        Return:
+            list of lists, with words from each of the processed files
+    """
+    files = reuters.fileids(category)
+    return [[START_TOKEN] + [w.lower() for w in list(reuters.words(f))] + [END_TOKEN] for f in files]
+reuters_corpus = read_corpus()
