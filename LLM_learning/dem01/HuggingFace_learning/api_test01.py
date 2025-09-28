a78fe49202fecbe1cecc711d5cf3124c23f817d2
躺平加菲猫 <10709382+lai_cat@user.noreply.gitee.com>
2025-09-28 20:40:27 +0800

    0928

--- LLM_learning/dem01/HuggingFace_learning/api_test01.py
+++ LLM_learning/dem01/HuggingFace_learning/api_test01.py
@@ -2,7 +2,7 @@
 # #使用Token访问在线模型
 #
 # API_URL = "https://api-inference.huggingface.co/models/uer/gpt2-chinese-cluecorpussmall"
-# #API_TOKEN = "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"
+# #API_TOKEN = "123"
 # headers = {"Authorization": f"Bearer {API_TOKEN}"}
 #
 # response = requests.post(API_URL,headers=headers,json={"inputs":"你好，Hugging face"})
